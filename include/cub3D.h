55a57b99dda9f2b77c03aed40b3c5359b43dba62
Tom Lebon <tlebon@paul-f3Br7s2.clusters.42paris.fr>
2025-02-06 17:43:39 +0100

    Debut de main + check pour l'ouverture du fichier de map (check extension et existence mais check si c'est un directory sans stat jcp comment faire voila

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:03:07 by tlebon            #+#    #+#             */
-/*   Updated: 2025/02/06 16:36:08 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/02/06 17:33:05 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 # define CUB3D_H
 
 #include "../libs/libft/includes/libft.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 
+typedef enum e_bool {TRUE, FALSE}t_bool;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,18 +6,48 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:02:37 by tlebon            #+#    #+#             */
-/*   Updated: 2025/02/06 16:36:19 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/02/06 17:41:50 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+
+t_bool	check_extension(char *path, char *extension)
+{
+	size_t	p_size;
+	size_t	e_size;
+	char	*ext_comp;
+
+	if (!path || !extension)
+		return (FALSE);	
+	p_size = ft_strlen(path);
+	e_size = ft_strlen(extension);
+	ext_comp = ft_substr(path, p_size - e_size, e_size);
+	if (ft_strncmp(extension, ext_comp, e_size) == 0)
+		return (free(ext_comp), TRUE);
+	return (free(ext_comp), FALSE);
+}
+
 int	main (int ac, char **av)
 {
+	int	fd;
+
 	if (ac != 2)
+	{
 		ft_printf("Usage : ./cub3D valid_map_path\nThanks\n");
-	av = NULL;
-	char	*test = get_next_line(0);
-	ft_printf("U typed in %s", test);
+		return (1);
+	}
+	if (check_extension(av[1], ".cub") == FALSE)
+	{
+		ft_putstr_fd("Wrong file. You need to select a .cub file\n", 2);
+		return (2);
+	}
+	fd = open(av[1], O_RDONLY); // Comment check si c'est bien un fichier et non un directory ?
+	if (fd < 0)	
+	{
+		perror("Open failed");
+		return (3);
+	}
 	return (0);
 }
